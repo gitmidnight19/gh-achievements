04abb25850214c1e09495c5bdade4594aa260e7a
JPM Tech <161664325+gitmidnight19@users.noreply.github.com>
2026-08-30 23:47:18 -0500

    feat: export the public library surface (#20)
    One entry point so the package can be used as a library, not only as a CLI.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { report } from "./report.js";
+export { createClient, GitHubError } from "./github/client.js";
+export { all as achievements, get as achievement } from "./achievements/index.js";
+export { formatTable } from "./format/table.js";
+export { formatJson } from "./format/json.js";
+export { formatMarkdown } from "./format/markdown.js";
