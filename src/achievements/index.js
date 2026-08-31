82079eecccdc06e348010a2b95da4cf7cf9820a4
JPM Tech <161664325+gitmidnight19@users.noreply.github.com>
2026-08-30 23:45:28 -0500

    feat: detect Starstruck (#12)
    Measured against the single most-starred repository the user owns. Forks are
    excluded, since the badge is for repositories you created.

--- src/achievements/index.js
+++ src/achievements/index.js
@@ -2,6 +2,7 @@ import "./pull-shark.js";
 import "./yolo.js";
 import "./quickdraw.js";
 import "./pair-extraordinaire.js";
+import "./starstruck.js";
 
 export { all, get, register, reset } from "./registry.js";
 export { resolveTier, formatTier } from "./tiers.js";
